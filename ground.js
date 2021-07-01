7281f2c66b5cf8e51e971334a2119a8f0d5342ab
ISHITA-SHAW <83875590+ISHITA-SHAW@users.noreply.github.com>
2021-07-01 21:16:49 +0530

    Update ground.js

--- ground.js
+++ ground.js
@@ -1,10 +1,11 @@
 class Ground {
-constructor() {
-    var options = {
-    isStatic: true
+    constructor() {
+        var options = {
+            isStatic: true
+        }
+        this.ground= Bodies.rectangle (450, 390, 900, 20, options); 
+        World.add(world, this.ground);
     }
-    this.ground= Bodies.rectangle (450, 390, 900, 20, options); World.add(world, this.ground);
-     }
     
     display(){
     strokeWeight (2);
@@ -12,3 +13,4 @@ constructor() {
     rectMode (CENTER);
     rect(this.ground. position.x,this.ground. position.y, 900, 20);
     }
+}
